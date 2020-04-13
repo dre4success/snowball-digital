c9b3a00e3599155379e1333bd90580b387caa29b
dre4success <dre4success@live.com>
2020-04-13 17:12:40 +0100

    from hostname

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const AWS = require('aws-sdk');
-const fileUpload = require('express-fileupload');
-const Jimp = require('jimp');
-const path = require('path');
-const crypto = require('crypto');
-require('dotenv').config({ path: 'variable.env' });
+const express = require("express");
+const AWS = require("aws-sdk");
+const fileUpload = require("express-fileupload");
+const Jimp = require("jimp");
+const path = require("path");
+const crypto = require("crypto");
+require("dotenv").config({ path: "variable.env" });
+const { hostname } = require("os");
 
 const app = express();
 
-let logoPath = path.join(__dirname + '/logo/snowball-digital.png');
+let logoPath = path.join(__dirname + "/logo/snowball-digital.png");
 
 app.use(fileUpload());
 
 AWS.config.update({
   accessKeyId: process.env.KEY_ID,
   secretAccessKey: process.env.ACCESS_KEY,
-  region: 'eu-west-1'
+  region: "eu-west-1",
 });
 
 const s3 = new AWS.S3();
 
-let message = `Hello Cloud \n`
+let message = `Hello Cloud from ${hostname()}`;
 
-app.get('/', (req, res) => {
-  res.json(message)
-})
+app.get("/", (req, res) => {
+  res.json(message);
+});
 
-app.post('/api/upload', async (req, res) => {
+app.post("/api/upload", async (req, res) => {
   try {
     // validating that it's a file being inputed
-    if (!req.files) throw Error('Please use form-data');
+    if (!req.files) throw Error("Please use form-data");
 
     // validating that it's the current name being used
     if (!req.files.image)
@@ -38,33 +39,37 @@ app.post('/api/upload', async (req, res) => {
         .json({ status: 400, message: `No image to upload` });
 
     // validating that only an image is being uploaded
-    if (!req.files.image.mimetype.includes('image/'))
+    if (!req.files.image.mimetype.includes("image/"))
       return res.status(400).json({
         status: 400,
-        message: `only images allowed for upload`
+        message: `only images allowed for upload`,
       });
 
-    let resource = `${await crypto.randomBytes(8).toString('hex')}.${
+    let resource = `${await crypto.randomBytes(8).toString("hex")}.${
       req.files.image.mimetype
     }`;
 
     let [image, logo] = await Promise.all([
       Jimp.read(req.files.image.data),
-      Jimp.read(logoPath)
+      Jimp.read(logoPath),
     ]);
     // let logo = await Jimp.read(logoPath);
     let overlayedImage = await image
       .scale(1)
       // overlaying logo over uploaded image
-      .composite(logo.scale(0.3), 128 - 75, 128, [Jimp.BLEND_DESTINATION_OVER, 0.5, 0.5])
+      .composite(logo.scale(0.3), 128 - 75, 128, [
+        Jimp.BLEND_DESTINATION_OVER,
+        0.5,
+        0.5,
+      ])
       .getBufferAsync(Jimp.MIME_JPEG);
 
     const params = {
-      Bucket: 'snowball-digital',
-      ACL: 'public-read',
+      Bucket: "snowball-digital",
+      ACL: "public-read",
       Key: resource,
       Body: overlayedImage,
-      ContentType: req.files.image.mimetype
+      ContentType: req.files.image.mimetype,
     };
     // uploading to AWS S3 bucket
     const data = await s3.upload(params).promise();
