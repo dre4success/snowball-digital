62484b4ce3f1a4f35912fc43cd60797251e8b7e9
dre4success <dre4success@live.com>
2020-04-15 13:26:21 +0100

    adding custom domain

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -11,6 +11,7 @@ AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile dre4success \
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 CFN_BUCKET="$STACK_NAME-cfn-$AWS_ACCOUNT_ID"
+DOMAIN=dre4success.com
 
 # Generate a personal access token with repo and admin:repo_hook
 # permissions from https://github.com/settings/tokens
@@ -60,6 +61,7 @@ aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     EC2InstanceType=$EC2_INSTANCE_TYPE \
+    Domain=$DOMAIN \
     GitHubOwner=$GH_OWNER \
     GitHubRepo=$GH_REPO \
     GitHubBranch=$GH_BRANCH \
